617ad479d87b3849076a4ab9b687aa0bb7dac427
GarifullinII <ildaridrisovich@gmail.com>
2021-10-26 23:38:19 +0400

    add info in Category component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import Header from "./Header";
+import Home from "./Home";
+import About from "./About";
+import Category from "./Category";
 import Footer from "./Footer";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 function App() {
   return (
     <>
       <Header/>
       <Footer/>
+        <Router>
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/about" component={About} />
+                <Route exact path="/cat" component={Category} />
+            </Switch>
+        </Router>
     </>
   );
 }
